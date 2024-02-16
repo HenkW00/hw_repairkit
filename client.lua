461d43e4cbbb218e28e84f3324383d76dec91653
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-16 23:15:20 +0100

    Initial commit

---
+++ client.lua
@@ -0,0 +1,53 @@
+Citizen.CreateThread(function()
+    RegisterNetEvent('hw_repairkit:repairVehicle')
+    AddEventHandler('hw_repairkit:repairVehicle', function()
+        local playerPed = PlayerPedId()
+        local vehicle, distance = ESX.Game.GetClosestVehicle()
+
+        if IsPedOnFoot(playerPed) and distance and distance < 3.0 then
+            local initialPos = GetEntityCoords(playerPed)
+            local vehiclePos = GetEntityCoords(vehicle)
+
+            if Vdist(initialPos, vehiclePos) < 3.0 then
+                if Config.Debug then
+                    print("hw_repairkit: Starting repair process.")
+                end
+
+                -- Freeze the player and play the repair animation
+                FreezeEntityPosition(playerPed, true)
+                ESX.Streaming.RequestAnimDict('mini@repair', function()
+                    TaskPlayAnim(playerPed, 'mini@repair', 'fixing_a_ped', 8.0, -8, -1, 49, 0, false, false, false)
+                end)
+
+                exports['okokNotify']:Alert("Vehicle Repair", "Voertuig aan het repareren...", Config.RepairTime, 'info')
+
+                local endTime = GetGameTimer() + Config.RepairTime
+                local repairCancelled = false
+                while GetGameTimer() < endTime do
+                    Citizen.Wait(100) -- Short wait to reduce script load
+                    local currentPos = GetEntityCoords(playerPed)
+                    if not IsEntityPlayingAnim(playerPed, 'mini@repair', 'fixing_a_ped', 3) or IsPedInAnyVehicle(playerPed, false) or Vdist(initialPos, currentPos) > 1.0 then
+                        exports['okokNotify']:Alert("Vehicle Repair", "Voertuig reparatie gestopt!", 5000, 'error')
+                        repairCancelled = true
+                        break
+                    end
+                end
+
+                -- Cleanup: stop the animation, unfreeze the player, and either complete or cancel the repair
+                ClearPedTasks(playerPed)
+                FreezeEntityPosition(playerPed, false)
+
+                if not repairCancelled then
+                    SetVehicleFixed(vehicle)
+                    exports['okokNotify']:Alert("Vehicle Repair", "Jouw voertuig is gerepareerd!", 5000, 'success')
+                end
+            else
+                exports['okokNotify']:Alert("Vehicle Repair", "Je moet dichterbij het voertuig staan.", 5000, 'error')
+            end
+        else
+            exports['okokNotify']:Alert("Vehicle Repair", "Geen voertuig in de buurt", 5000, 'error')
+        end
+    end)
+end)
+
+
